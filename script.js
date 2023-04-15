1095adb4ae3b018c0d9d640fc268e10b1e106d9d
ronakraj00 <12213052@nitkkr.ac.in>
2023-04-15 20:28:34 +0530

    Make button for colors

--- script.js
+++ script.js
@@ -29,30 +29,62 @@ function createDiv(id){
 
 //hover effect
 //Mouse Over
-function changeColor(){
+function changeColor(evt){
+    console.log(evt);
+
     const rows=document.querySelectorAll('.row');
     const cols=document.querySelectorAll('.col');
-    cols.forEach(col=>{col.addEventListener('mouseover',()=>{
-    col.classList.add('colColor');
+    if(evt=='rColor'){
+        cols.forEach(col=>{col.addEventListener('click',()=>{
+            // col.classList.add('colColor');
+            randomColor=Math.floor(Math.random()*16777215).toString(16);
+            col.style.backgroundColor = "#" + randomColor;
+        
+        })});
+    }
+    if(evt=='wColor'){
+        cols.forEach(col=>{col.addEventListener('click',()=>{
+            col.style.backgroundColor = 'white';
+        
+        })});
+    }
+    if(evt=='bColor'){
+        cols.forEach(col=>{col.addEventListener('click',()=>{
+    col.style.backgroundColor ='black';
+
 
 })});
+    }
+    
 //Mouse Out
-cols.forEach(col=>{col.addEventListener('mouseout',()=>{
-    col.classList.remove('colColor');
-})})}
+// cols.forEach(col=>{col.addEventListener('mouseout',()=>{
+//     col.classList.remove('colColor');
+// })})
+}
 
 //working start from here
 //Size button working
 const boxSizes=document.querySelectorAll('.boxSize');
 boxSizes.forEach(boxSize=>{boxSize.addEventListener('click',(evt)=>{
-    console.log(evt.target.id);
-
+    // console.log(evt.target.id);
+    boxSize.classList.toggle('buttonColor');
     resetDiv();
     
     createDiv(evt.target.id);
-    changeColor();
+    // changeColor(evt);
     evt.stopPropagation();
 })});
+
+//color button working
+const funButtons=document.querySelectorAll(".funButton");
+funButtons.forEach(funButton=>{
+    funButton.addEventListener("click",(evt)=>{
+        funButton.classList.toggle('buttonColor');
+        changeColor(evt.target.id);
+        evt.stopPropagation();
+    })
+})
+
 
 //function to reset div
 function resetDiv(){
