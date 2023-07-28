ae7766ef788095d47b8c069815740c4436321518
ronak raj <126279165+ronakraj00@users.noreply.github.com>
2023-07-28 15:56:32 +0530

    new ui

--- script.js
+++ script.js
@@ -1,95 +1,75 @@
 //DOM manipulation
-const main=document.querySelector('#main');
-const container=document.querySelector('#container');
-
+const main = document.querySelector("#main");
 
 //create div's
-function createDiv(id){
+function createDiv(id) {
+  const container = document.createElement("div");
+  container.setAttribute("id", "container");
+  for (let i = 1; i <= id; i++) {
+    const rowDiv = document.createElement("div");
+    rowDiv.classList.add("row");
 
-    for(let i=1;i<=id;i++){
-
-        const rowDiv=document.createElement('div');
-        rowDiv.classList.add('row');
-
-        for( let j=1;j<=id;j++){
-
-            const divCol=document.createElement('div');
-            divCol.classList.add('col');
-            divCol.classList.add(`col${id}`);
-            rowDiv.appendChild(divCol);
-        }
-
-        container.appendChild(rowDiv);
+    for (let j = 1; j <= id; j++) {
+      const divCol = document.createElement("div");
+      divCol.classList.add("col");
+      divCol.classList.add(`col${id}`);
+      rowDiv.appendChild(divCol);
     }
+    container.appendChild(rowDiv);
+  }
+  main.append(container);
 }
-//dom rows and cols selector
-
-
-
 
 //hover effect
 //Mouse Over
-function changeColor(evt){
-    console.log(evt);
-
-    const rows=document.querySelectorAll('.row');
-    const cols=document.querySelectorAll('.col');
-    if(evt=='rColor'){
-        cols.forEach(col=>{col.addEventListener('mouseover',()=>{
-            // col.classList.add('colColor');
-            randomColor=Math.floor(Math.random()*16777215).toString(16);
-            col.style.backgroundColor = "#" + randomColor;
-        
-        })});
-    }
-    if(evt=='wColor'){
-        cols.forEach(col=>{col.addEventListener('mouseover',()=>{
-            col.style.backgroundColor = 'azure';
-        
-        })});
-    }
-    if(evt=='bColor'){
-        cols.forEach(col=>{col.addEventListener('mouseover',()=>{
-    col.style.backgroundColor ='black';
-
-
-})});
-    }
-    
-//Mouse Out
-// cols.forEach(col=>{col.addEventListener('mouseout',()=>{
-//     col.classList.remove('colColor');
-// })})
+function changeColor(evt) {
+  const cols = document.querySelectorAll(".col");
+  if (evt == "random-color") {
+    cols.forEach((col) => {
+      col.addEventListener("mouseover", () => {
+        randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        col.style.backgroundColor = "#" + randomColor;
+      });
+    });
+  } else {
+    cols.forEach((col) => {
+      col.addEventListener("mouseover", () => {
+        col.style.backgroundColor = evt;
+      });
+    });
+  }
 }
 
 //working start from here
 //Size button working
-const boxSizes=document.querySelectorAll('.boxSize');
-boxSizes.forEach(boxSize=>{boxSize.addEventListener('click',(evt)=>{
-    // console.log(evt.target.id);
-    boxSize.classList.toggle('buttonColor');
-   resetDiv();
-    
+const boxSizes = document.querySelectorAll(".boxSize");
+boxSizes.forEach((boxSize) => {
+  boxSize.addEventListener("click", (evt) => {
+    funButtons.forEach((button) => button.classList.remove("active-button"));
+    boxSizes.forEach((boxSize) => boxSize.classList.remove("active-button"));
+    boxSize.classList.add("active-button");
+    resetDiv();
     createDiv(evt.target.id);
-    // changeColor(evt);
     evt.stopPropagation();
-})});
+  });
+});
 
 //color button working
-const funButtons=document.querySelectorAll(".funButton");
-funButtons.forEach(funButton=>{
-    funButton.addEventListener("click",(evt)=>{
-        funButton.classList.toggle('buttonColor');
-        changeColor(evt.target.id);
-        evt.stopPropagation();
-    })
-})
-
+const funButtons = document.querySelectorAll(".funButton");
+funButtons.forEach((funButton) => {
+  funButton.addEventListener("click", (evt) => {
+    funButtons.forEach((button) => button.classList.remove("active-button"));
+    funButton.classList.add("active-button");
+    changeColor(evt.target.id);
+    evt.stopPropagation();
+  });
+});
 
 //function to reset div
-function resetDiv(){
-    while(container.hasChildNodes()){
-        container.removeChild(container.firstChild);
+function resetDiv() {
+    main.textContent="";
+    const container=document.querySelector("#container");
+    if(container){
+        container.remove();
     }
-    
 }
